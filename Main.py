1f2fc75f8ca0db556d6617aa1169e8f20077db39
Y Sai Koushik Reddy <152732336+KoushikReddy9963@users.noreply.github.com>
2025-06-13 12:25:25 +0530

    Making a clear notes

---
+++ Main.py
@@ -0,0 +1,45 @@
+#Introduction
+print("Hello world")
+
+# conditonal statement
+if 5 > 4:
+    print("True")
+
+# multi 
+# line 
+# comments
+
+# specify data type using casting 
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x,y,z) # output 3 3 3.0
+
+
+#functions
+x = "awesome" #redeclared global versions
+# also u can use global
+
+def function_name():
+    print(x)
+
+function_name()
+
+
+# slice in the strings b[2:5]  from position 2 to position 5 (not included):
+print(x[2:5])
+
+# it also includes variations like - minus , empty : 
+
+# .upper() .lower()  .replace('x','y')  .split(",")
+
+print("Hello {y}") # it replaces 3
+
+# algorithimic operators we already know + - / * % u can use 
+# bitwise operators too ^ & | ! ~
+
+# Now lets go to data types in Python programming language
+#List Tuple Set Dictionary
+
+
